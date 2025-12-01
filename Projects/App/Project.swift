751107311324510ba2933687bb00f7c1aeb692b3
Conner <dudrjs145@naver.com>
2025-12-02 00:26:34 +0900

    fix: Scheme 설정 버그 수정

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -9,32 +9,32 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.make(
-  name: "App",
-  targets: [
-    .make(
-      name: TargetName.app.rawValue,
-      product: .app,
-      bundleId: "com.conner.HyperFocus.App",
-      sources: ["Sources/**"],
-      dependencies: [
-        .project(target: .common, projectPath: .core),
-        .project(target: .designSystem, projectPath: .shared),
-      ]
-    ),
-  ],
-  schemes: [
-    Scheme(
-      name: TargetName.app.rawValue,
-      shared: true,
-      buildAction: BuildAction(
-        targets: [
-          TargetReference.target(name: TargetName.app.rawValue),
-        ]
-      ),
-      runAction: RunAction(
-        configuration: .debug,
-        executable: .target(name: TargetName.app.rawValue)
-      )
-    ),
-  ]
+    name: "App",
+    targets: [
+        .make(
+            name: TargetName.app.rawValue,
+            product: .app,
+            bundleId: "com.conner.HyperFocus.App",
+            sources: ["Sources/**"],
+            dependencies: [
+                .project(target: .common, projectPath: .core),
+                .project(target: .designSystem, projectPath: .shared),
+            ]
+        ),
+    ],
+    schemes: [
+        .scheme(
+            name: "Production",
+            shared: true,
+            buildAction: .buildAction(
+                targets: [
+                    .target(TargetName.app.rawValue)
+                ]
+            ),
+            runAction: .runAction(
+                configuration: .debug,
+                executable: .init(.target(TargetName.app.rawValue))
+            )
+        )
+    ]
 )
