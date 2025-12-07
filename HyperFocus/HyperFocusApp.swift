bd40ceaa21eee4b5bd186adb22a290abc61f1b1d
Conner <dudrjs145@naver.com>
2025-12-07 23:32:33 +0900

    Initial Commit

---
+++ HyperFocus/HyperFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperFocusApp.swift
+//  HyperFocus
+//
+//  Created by 김영건 on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HyperFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
