8058094b9fa3d3aa6ce7aff0b57ca4646feb48d6
Conner <dudrjs145@naver.com>
2026-01-12 02:27:03 +0900

    chore: xcode cloud script 추가

---
+++ HyperFocus/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,6 @@
+
+set -euo pipefail
+
+
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
